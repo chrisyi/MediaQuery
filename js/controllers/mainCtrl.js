8be682b3751ebbb5c56d556a7d712aea0ff2b388
cy <cy@Christophers-MacBook-Pro.local>
2017-06-12 18:47:17 -0500

    home bg and search bar

--- js/controllers/mainCtrl.js
+++ js/controllers/mainCtrl.js
@@ -4,8 +4,23 @@ angular.module('app')
 
         $scope.getMovieTitleData = function (movieTitleParam) {
             // console.log(movieTitleParam)
-            $state.go('searchresult', {movie: movieTitleParam})
+            $state.go('searchresult', { movie: movieTitleParam })
         }
+
+        
+        
+
+        mainSrv.getPopularMovieBackground().then(function (response) {
+             var random = response[Math.floor(Math.random()*response.length)]
+             var bgImages = document.getElementById("bgImages");
+             bgImages.style.backgroundImage = "url("+ random +")";
+             bgImages.style.height = "100vh";
+             bgImages.style.width = "100vw";
+             bgImages.style.backgroundSize = "cover";
+             bgImages.style.repeat = "no-repeat"
+             bgImages.style.backgroundPosition = "top center"
+        })
+
     })
 
 
--- js/services/mainService.js
+++ js/services/mainService.js
@@ -1,6 +1,31 @@
 angular.module('app')
     .service('mainSrv', function ($http, $q) {
 
+
+        //service for home
+        this.getPopularMovieBackground = function () {
+            return $http.get('https://api.themoviedb.org/3/movie/popular?api_key=488445c2f5175ddb3f6e335c1e55ce4c&language=en-US&page=1')
+                .then(function (response) {
+                    var bgArray = [];
+                    var results = response.data.results;
+                    for (var i = 0; i < results.length; i++) {
+                        bgArray.push('https://image.tmdb.org/t/p/original' + results[i].poster_path)
+                    }
+                    console.log(bgArray)
+                    return bgArray;
+                })
+        }
+
+        // this.randomImage = function () {
+            
+        //     this.bgArray[Math.floor(Math.random()*bgArray.length)]
+        // }
+
+
+
+
+
+
         //service for search-result
         this.getMovieTitleData = function (searchQuery) {
             return $http.get('https://api.themoviedb.org/3/search/movie?api_key=488445c2f5175ddb3f6e335c1e55ce4c&language=en-US&query='
@@ -23,25 +48,26 @@ angular.module('app')
                 })
         }
 
+
         //service for movie-choice
-        this.getMovieIdData = function(id) {
-            var promise = new Promise(function(resolve, reject ) {
+        this.getMovieIdData = function (id) {
+            var promise = new Promise(function (resolve, reject) {
                 var results = []
                 $http.get('https://api.themoviedb.org/3/movie/' + id + '?api_key=488445c2f5175ddb3f6e335c1e55ce4c')
-                .then(function(response) {
-                    results.push(response.data)
-                    if (results.length > 1) {
-                        resolve(Object.assign({}, results[0], results[1]))
-                    }
-                })
+                    .then(function (response) {
+                        results.push(response.data)
+                        if (results.length > 1) {
+                            resolve(Object.assign({}, results[0], results[1]))
+                        }
+                    })
                 $http.get('https://api.themoviedb.org/3/movie/' + id + '/credits?api_key=488445c2f5175ddb3f6e335c1e55ce4c')
-                .then(function(response) {
-                    results.push(response.data)
-                    if (results.length > 1) {
-                        resolve(Object.assign({}, results[0], results[1]))
-                    } 
-                })
-                })
+                    .then(function (response) {
+                        results.push(response.data)
+                        if (results.length > 1) {
+                            resolve(Object.assign({}, results[0], results[1]))
+                        }
+                    })
+            })
             return promise
 
         }
